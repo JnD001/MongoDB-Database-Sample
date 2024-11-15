2d7be43bf1b53417a76f608d8c4f0f639e59bce0
JnD001 <140713958+JnD001@users.noreply.github.com>
2024-11-15 12:36:45 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const { MongoClient } = require('mongodb');
 
-// Replace <username> and <password> with your MongoDB credentials
+
 const uri = 'mongodb+srv://*#######*.grwuh.mongodb.net/Login?retryWrites=true&w=majority';
 
 const client = new MongoClient(uri);
@@ -12,7 +12,7 @@ async function connectDB() {
     try {
       await client.connect();
       console.log("Connected to MongoDB");
-      db = client.db('Login'); // Your database name
+      db = client.db('Login'); 
     } catch (error) {
       console.error("Error connecting to MongoDB:", error);
       process.exit(1); // Exit if there's a connection error
